9eab51f60e28fba8db102851522b19e2b35cd101
Ahmed Orabi <eng.ahmedorabi94@gmail.com>
2023-05-08 02:36:12 +0300

    update some flavors

--- app/src/main/java/com/example/bbcnewsapp/core/data/ApiService.kt
+++ app/src/main/java/com/example/bbcnewsapp/core/data/ApiService.kt
@@ -1,13 +1,15 @@
 package com.example.bbcnewsapp.core.data
 
+import com.example.bbcnewsapp.BuildConfig
 import com.example.bbcnewsapp.core.domain.models.NewsResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
-//interface ApiService {
-//
-//
-//    @GET("top-headlines?sources=bbc-news&apiKey=499c1384b1f1467c90f8276feeb2a2ba")
-//    suspend fun getNewsResponseAsync(): NewsResponse
-//
-//
-//}
+interface ApiService {
+
+
+    @GET("top-headlines?apiKey=499c1384b1f1467c90f8276feeb2a2ba")
+    suspend fun getNewsResponseAsync(@Query("sources") sources: String = BuildConfig.sources): NewsResponse
+
+
+}
